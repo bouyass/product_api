bfd2f842384102bc39c2b2e95f45def7e61cd5f5
lyes makhloufi <lyes.makhloufi@outlook.fr>
2020-08-21 22:18:40 +0200

    lyes - product api - init project ( server, app)

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const parser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(parser.json());
+
+mongoose.connect('mongodb+srv://lyes:root@cluster0.crezr.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {console.log("connexion réussie")})
+    .catch((error) => console.log("connexion échouée"));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+})
+
+
+module.exports =  app;
